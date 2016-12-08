99c4d2dc9fed739fc1ec60a2070b63597e9d5263
kavitadaga <kadaga1@hotmail.com>
2016-12-08 14:59:34 -0500

    comments on the program

---
+++ NewDeckCards/NewDeckCards/NewDeckCards.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewDeckCards
+{
+    class NewDeckCards
+    {
+        string face;
+        int value;
+        string suit;
+        bool played;
+
+        public NewDeckCards()
+        {
+            
+        }
+
+        public NewDeckCards(string Face, int Value, string Suit)
+        {
+            face = Face;
+            value = Value;
+            suit = Suit;
+        }
+        public string getFace()
+        {
+            string getFace = face;
+            return face;
+        }
+        public void SetFace(string Face)
+        {
+            face = Face;
+        }
+        public void SetVal(int Val)
+        {
+            value = Val;
+        }
+        public void printInfo()
+        {
+            if (value == 11 || value == 24  || value == 37 || value == 50)
+                Console.WriteLine("face:{0}  {1}  {2}", face, "Jack" , suit);
+            else if (value == 12 || value == 25 || value == 38 || value == 51)
+                Console.WriteLine("face:{0} {1} {2}", face, "Queen", suit);
+            else if (value == 13 || value == 26 || value == 39 || value == 52)
+                Console.WriteLine("face:{0} {1}  {2}", face, "King", suit);
+            else if (value == 1 || value == 14 || value == 27 || value == 40)
+                Console.WriteLine("face:{0}  {1} {2}", face, "Ace", suit);
+            else 
+            Console.WriteLine("face:{0}  {1}", face, suit);
+        }
+
+    }
+
+    
+
+}
---
+++ NewDeckCards/NewDeckCards/Program.cs
@@ -0,0 +1,91 @@
+﻿//Prob11 Chapter 8 Ward Card Game
+
+using System;
+using static System.Console;
+
+namespace NewDeckCards
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            NewDeckCards[] myCard = new NewDeckCards[52];
+            string[] suitType = { "spade", "heart", "diamond", "clubs" };
+
+            int[] computer = new int[52];           //Array to store random numbers generated
+            int[] player = new int[26];
+            int[] computer1 = new int[26];
+            int j = 0, k = 0;
+            int fx =0, temp, playerPoints = 0, ComputerPoints = 0;
+          
+            Random rnd = new Random();
+            //For each card x gives spade ,heart --- clubs face and i assings value from 1- 13 ,1-Ace,13-King.
+            for (int x = 0; x <= 3; x++)
+            {
+                for (int i = 0; i < 13; i++)
+                {
+                   //Assigning a unique value to each card by index j
+                    myCard[j] = new NewDeckCards(Face: Convert.ToString(i + 1), Value: j + 1, Suit: suitType[x]);
+                    // if(i == 0) { myCard[j].SetFace("Ace");  };
+                    myCard[j].printInfo();
+                    j++;
+                }
+            }
+            //Displaying the Array of 52 Objects in an array.
+            for (k = 0; k < 52; k++)
+            {
+                myCard[k].printInfo();
+                computer[k] = k;
+            }
+            //52 values stored in computer Array which represents computer
+            // Shuffle the array
+            
+            for (k = 0; k < 26; k ++)
+            {
+                fx = rnd.Next(0, 52);
+                temp = computer[51-k];
+                computer[51-k] = computer[fx];
+                computer[fx] = temp;
+                WriteLine("**************" + computer[k]);
+
+            }
+                //Player array stores from values 0-26
+              //new computer array from 26
+
+            for (k = 0; k < 26; k++)
+            {
+                
+                player[k] = computer[k];
+                computer1[k] = computer[k + 25];
+                string a = myCard[player[k]].getFace();//getface method call
+                string b = myCard[computer1[k]].getFace();
+                if (a == b)
+                {
+                    WriteLine("Tie");
+                    playerPoints += 1;
+                    ComputerPoints += 1;
+                }
+                else if (int.Parse(a) > int.Parse(b))
+                {
+                    WriteLine(" Player score:{0} and Computer Score:{1}", a, b);
+                    playerPoints += 2;
+                    ComputerPoints += 0;
+                }
+                else
+                {
+                    WriteLine(" Computer Score:{0} and Player Score :{1}", b, a);
+                    playerPoints += 0;
+                    ComputerPoints += 2;
+                }
+             
+            }
+            WriteLine("Player:{0} Computer:{1}" ,playerPoints ,ComputerPoints);
+            WriteLine(playerPoints - ComputerPoints > 0 ?  playerPoints : ComputerPoints);
+
+
+        }
+    }
+}
+
